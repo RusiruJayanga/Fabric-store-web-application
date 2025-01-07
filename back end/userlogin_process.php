170a4d042863af363474c72a89884a75710c3d1a
RusiruJayanga <rusirujayanga@gmail.com>
2025-01-07 13:14:53 +0530

    Authentication
    Customer and admin authentication is complete.

---
+++ back end/userlogin_process.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+   
+    $name = $_POST['your_name'];
+    $password = $_POST['your_pass'];
+
+    
+    $servername = "localhost";
+    $db_username = "root";
+    $db_password = "";
+    $dbname = "rusiru";
+
+    $conn = new mysqli($servername, $db_username, $db_password, $dbname);
+
+    
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    
+    $sql = "SELECT * FROM user WHERE name='$name' AND password='$password'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows == 1) {
+        
+        $_SESSION['uname'] = $name;
+       
+       // header("Location: ");
+        exit();
+    } else {
+       
+        echo "Invalid user name or password.";
+        
+    }
+
+    $conn->close();
+}
+?>
